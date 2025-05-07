ce1b9a644a8fce5c1c260ab886e4a1d9e8b4be91
Marcos Medina <111904987+mmedina2@users.noreply.github.com>
2025-05-07 14:56:24 -0500

    feat: Add RouteTracker component for page tracking and remove Add Clothes link from Navbar

--- armarium/src/components/Navbar.js
+++ armarium/src/components/Navbar.js
@@ -68,11 +68,6 @@ function Navbar() {
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" id="add-clothes-link" to="/add-clothes" end>
-              Add Clothes
-            </NavLink>
-          </li>
-          <li className="nav-item">
             <button className="nav-link btn btn-link" id="signout-button" onClick={handleLogout}>
               Sign Out
             </button>
