044d8e64cfe02e1101bb914be09bba170eed12ba
Diana N <100163013+dianan64@users.noreply.github.com>
2025-05-08 11:29:23 -0500

    chore: Fixed styling

--- armarium/src/components/profile/Bookmarked.js
+++ armarium/src/components/profile/Bookmarked.js
@@ -56,39 +56,39 @@ function Bookmarked() {
         </button>
         <h1>Your Bookmarked Styleboards</h1>
         {bookmarkedStyleboards.length > 0 ? (
-          <ul className="styleboards-list">
+          <ul className="styleboards-list4">
             {bookmarkedStyleboards.map((styleboard) => {
               console.log('Styleboard Data:', styleboard); 
               return (
                 <li
                   key={styleboard.id}
-                  className="styleboard-item"
+                  className="styleboard-item4"
                   onClick={() => handleStyleboardClick(styleboard.id)}
                   style={{ cursor: 'pointer' }} 
                 >
                   <h2>{styleboard.name || 'Unnamed Styleboard'}</h2>
-                  <div className="outfits-preview">
+                  <div className="outfits-preview4">
                     {styleboard.outfits?.[0] && ( 
-                      <div className="outfit-preview">
+                      <div className="outfit-preview4">
                         {styleboard.outfits[0].images?.top && (
                           <img
                             src={styleboard.outfits[0].images.top}
                             alt="Top"
-                            className="outfit-image"
+                            className="outfit-image4"
                           />
                         )}
                         {styleboard.outfits[0].images?.bottom && (
                           <img
                             src={styleboard.outfits[0].images.bottom}
                             alt="Bottom"
-                            className="outfit-image"
+                            className="outfit-image4"
                           />
                         )}
                         {styleboard.outfits[0].images?.shoes && (
                           <img
                             src={styleboard.outfits[0].images.shoes}
                             alt="Shoes"
-                            className="outfit-image"
+                            className="outfit-image4"
                           />
                         )}
                       </div>
